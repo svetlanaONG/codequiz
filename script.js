b59ca078d717886606e75ee6caa7a54e337103e2
svetlanaONG <lana.milova2017@gmail.com>
2021-11-13 11:21:56 -0700

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+var x = 50;
+var expression1 = x < 25;
+var expression2 = x > 50;
+console.log (expression1, expression2)
+if (expression1 && expression2) {
+    console.log("True true")
+}
+else if (expression) {
+    console.log ("True False")
+}
